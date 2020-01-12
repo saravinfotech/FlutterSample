18e77e18cceb43adcbdaac7f8137dc7bdc0d3ed8
154773 <saravanan6.kumar6@cognizant.com>
2020-01-12 01:36:13 +0000

    Flutter Images from assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: //Image.network(
+            //'https://www.w3schools.com/w3css/img_lights.jpg'
+      Image(
+        image: AssetImage("images/diamond.png"),
+      )
+      ),
       appBar: AppBar(
         title: Text("Nothing Special"),
         backgroundColor: Colors.blueGrey[900],
